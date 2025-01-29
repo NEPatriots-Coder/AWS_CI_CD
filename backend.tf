3937fe84c06941a958c9cf20adb47ec746e1f928
Lamar Wells Jr <lamar.wells@smail.rasmussen.edu>
2025-01-28 22:33:47 -0600

    Update backend.tf
    Cleaned up file

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "cicd-terraform-state-lw"
     key            = "terraform.tfstate"
-    region         = "us-east-2"  
+    region         = "us-east-2"
     encrypt        = true
     dynamodb_table = "terraform-state-lock"
   }
